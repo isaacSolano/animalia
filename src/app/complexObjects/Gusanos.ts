b55df6c301b4cd4057ed12d1fa266e8cf751768b
Isaac Solano <isolanol@ucenfotec.ac.cr>
2020-05-29 20:13:42 -0600

    Creacion de clases: Esponjas y Gusanos

---
+++ src/app/complexObjects/Gusanos.ts
@@ -0,0 +1,43 @@
+import { Invertebrado } from './Invertebrados';
+
+export class Gusano extends Invertebrado {
+    private _TipoAlimentacion;
+    private _TipoMovimiento;
+    private _TipoComunicacion;
+
+    constructor(pNombre:String, pSonido:String, pTipoEsqueleto:String, pCantidadExtremidades:Number, pTipoAlimentacion:String, pTipoMovimiento:String, pTipoComunicacion:String){
+        super(pNombre, pSonido, pTipoEsqueleto, pCantidadExtremidades);
+
+        this._TipoAlimentacion = pTipoAlimentacion;
+        this._TipoMovimiento = pTipoMovimiento;
+        this._TipoComunicacion = pTipoComunicacion;
+    }
+
+    set TipoAlimentacion(pTipoAlimentacion:String){
+        this._TipoAlimentacion = pTipoAlimentacion;
+    }
+
+    set TipoMovimiento(pTipoMovimiento:String){
+        this._TipoMovimiento = pTipoMovimiento;
+    }
+
+    set TipoComunicacion(pTipoComunicacion:String){
+        this._TipoComunicacion = pTipoComunicacion;
+    }
+
+    Alimentacion(){ 
+        return  `Nuestro ${this.Nombre.toLowerCase()} es ${this._TipoAlimentacion}`; 
+    }
+
+    Comunicacion(){
+        return `El sonido de ${this.Nombre.toLowerCase()} es ${this._TipoComunicacion}`;
+    }
+
+    Movimiento(){
+        return  `El movimiento de ${this.Nombre.toLowerCase()} es ${this._TipoMovimiento}`; 
+    }
+
+    sumary(){
+        return `${super.summary()}. Alimentacion => ${this.Alimentacion}. Comunicacion => ${this.Comunicacion}. Movimiento => ${this.Movimiento}`;
+    }
+}
